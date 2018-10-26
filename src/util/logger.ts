06de6aa1285c465ea6f29ae76ca0c34f875adb52
vippelina <vibeketengroth@gmail.com>
2018-10-26 13:55:58 +0200

    wrapper for logger added

---
+++ src/util/logger.ts
@@ -0,0 +1,13 @@
+import winston from 'winston'
+import {ENVIRONMENT} from './secrets'
+
+// inspiration
+// https://github.com/Microsoft/TypeScript-Node-Starter/blob/master/src/util/logger.ts
+winston.createLogger({
+    transports: [
+        new (winston.transports.Console)({
+            level: process.env.NODE_ENV === 'production' ? "error" : "debug"
+        }),
+        new (winston.transports.File)({filename: "debug.log", level: "debug"})
+    ]
+})
