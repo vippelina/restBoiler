a82254f5a041e63c09cd989dcb5cac7d9362f270
vippelina <vibeketengroth@gmail.com>
2018-10-28 13:57:13 +0100

    added validation for email

--- src/models/Account.ts
+++ src/models/Account.ts
@@ -1,7 +1,13 @@
 import {model, Schema} from 'mongoose';
 
 const AccountSchema: Schema = new Schema({
-    email: String
+    email: {
+        type: String,
+        trim: true,
+        validate: function(email: string) {
+            return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+        }
+    }
 })
 
 export default model('Account', AccountSchema)
